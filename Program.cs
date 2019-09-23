f515a967b58947a6f36ffce4dd845652cd5c91f4
Robertoz44 <roberto-m@live.se>
2019-09-23 15:11:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeLAB
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //DateTime myBirthday = DateTime.Parse("1992/01/30");
+            //TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+
+            //Console.WriteLine(myAge.TotalDays);
+
+            //Console.ReadLine();
+
+            //Lab1
+
+            //DateTime Lab = DateTime.Now;
+            //Console.WriteLine(Lab.ToString());
+
+            //DateTime Labb = DateTime.Now;
+            //Console.WriteLine(Labb.ToShortDateString());
+
+
+            //Lab2
+
+
+            //Console.WriteLine($"month = {dt.Month}");
+
+            //Console.WriteLine($"day = {dt.Day}");
+
+            //Console.WriteLine($"hour = {dt.Hour}");
+
+            //Console.WriteLine($"minute = {dt.Minute}");
+
+            //Console.WriteLine($"second = {dt.Second}");
+
+            //Console.WriteLine($"millisecond = {dt.Millisecond}");
+
+            //Console.ReadLine();
+
+            //Lab3
+
+            //DateTime dt = DateTime.Now;
+
+            //string veckoDagString = dt.DayOfWeek.ToString();
+
+            //Console.WriteLine($"Dagens veckodag är {veckoDagString}");
+
+
+            //DateTime dt = DateTime.Now;
+
+
+            //string inSwedish = "";
+
+            //switch (dt.DayOfWeek)
+            //{
+            //    case DayOfWeek.Monday:
+            //        inSwedish = "Måndag";
+            //        break;
+
+            //    case DayOfWeek.Tuesday:
+            //        inSwedish = "Tisdag";
+            //        break;
+
+            //    case DayOfWeek.Wednesday:
+            //        inSwedish = "Onsdag";
+            //        break;
+
+            //    case DayOfWeek.Thursday:
+            //        inSwedish = "Torsdag";
+            //        break;
+
+            //    case DayOfWeek.Friday:
+            //        inSwedish = "Fredag";
+            //        break;
+
+            //    case DayOfWeek.Saturday:
+            //        inSwedish = "Lördag";
+            //        break;
+
+            //    case DayOfWeek.Sunday:
+            //        inSwedish = "Söndag";
+            //        break;
+
+            //Lab4
+
+            //DateTime today = DateTime.Now;
+
+            //DateTime answer = today.AddDays(40);
+
+            //Console.WriteLine("Idag är det: {0:dddd}", today);
+            //Console.WriteLine("Om 40 dagar är det: {0:dddd}", answer);
+
+            //Console.WriteLine($"Idag är det den {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+
+            //DateTime dt = DateTime.Now.AddDays(40);
+
+            //Console.WriteLine($"Om 40 dagar så är det {dt.DayOfWeek.ToString()}");
+            //Console.WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss"));
+
+
+            //Console.ReadLine();
+
+            //Lab5
+
+            //DateTime dt = DateTime.Now;
+            //Console.WriteLine(dt.ToString("yyyy-MM-dd"));
+
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    dt = dt.AddDays(40);
+            //    Console.WriteLine(dt.ToString("yyyy-MM-dd"));
+            //}
+
+
+            //Console.ReadLine();
+
+            //Lab6
+
+            Console.WriteLine("Ange dagens datum: ");
+            string datum = Console.ReadLine();
+            
+
+            
+
+        }
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
